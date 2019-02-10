9df2a2a9fb3c8839b33320263309a72afbfaa434
Rafael Batista <rafaelb24@yahoo.com>
2019-02-10 02:26:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get 'name' do 
+  get '/name' do 
     "My name is Rafael"
   end 
   
-   get 'hometown' do 
+   get '/hometown' do 
     "My hometown is Bronx, New York"
   end 
   
-   get 'favorite-song' do 
+   get '/favorite-song' do 
     "My favorite song is Comfortably Numb by Pink Floyd"
   end
 end
