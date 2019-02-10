a28bf8176028ebe1d16fc0baa095594c8eb4620d
Rafael Batista <rafaelb24@yahoo.com>
2019-02-10 02:24:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get 'name' do 
     "My name is Rafael"
   end 
+  
+   get 'hometown' do 
+    "My hometown is Bronx, New York"
+  end 
+  
+   get 'favorite-song' do 
+    "My favorite song is Comfortably Numb by Pink Floyd"
+  end
 end
